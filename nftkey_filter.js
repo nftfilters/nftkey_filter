5edecc922fd8e86b2c97440534f24d2008b415a3
nftfilters <93408926+nftfilters@users.noreply.github.com>
2021-10-31 22:18:01 +0100

    Update nftkey_filter.js

--- nftkey_filter.js
+++ nftkey_filter.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         NFTKEY rarity filter
+// @name         NFTKEY filter
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  NFTKEY rarity filter
